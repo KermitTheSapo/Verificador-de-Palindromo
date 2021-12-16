9487b79b7e02ed72c943ba368aa9bc83c9b5776e
Robert <rjsf06@gmail.com>
2021-12-16 09:47:53 -0300

    main

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 function veficiaPalindromo(string) {
     if(!string) return;
 
-    string.split("").reverse().join("") === string;
+    return string.split("").reverse().join("") === string;
 }
 console.log (veficiaPalindromo("ovo"))
